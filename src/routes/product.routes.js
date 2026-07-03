62bf85fe8985d1e23911ab13f57b023a7c749de7
@debnathbablu <dn.bablu.2004@gmail.com>
2026-07-03 18:02:16 +0530

    Delete src directory

--- src/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")  
-const router = express.Router();
-
-const {createProduct, getProducts, getProductById, updateProduct, deleteProduct} = require("../controllers/product.controller")  
-
-router.route("/").get(getProducts).post(createProduct); 
-router.route("/:id").get(getProductById).put(updateProduct).delete(deleteProduct)  
-
-module.exports = router;
